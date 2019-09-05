775bd3b827b2fc18dd5436893930ace5b5aee858
unknown <17839192461@163.com>
2019-09-05 21:45:36 +0800

    解决冲突

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from apps.cms.models import Room, Floor
 
@@ -25,12 +25,19 @@ bp = Blueprint('front', __name__, url_prefix='/front')
 
 @bp.route('/')
 def index():
-    gender = 0
+    gender = request.args.get('gender')
+    if gender == 'boy':
+        sex = 0
+    elif gender == 'girl':
+        sex = 1
+    else:
+        return
     data = []
     floors = Floor.query.order_by(Floor.bid).order_by(Floor.floor_num).all()
     for floor in floors:
         floor_num = floor.floor_num
-        rooms = Room.query.filter_by(fid=floor.id, gender=gender).all()
+        floor_num = '%02d' % floor_num
+        rooms = Room.query.filter_by(fid=floor.id, gender=sex).all()
         seats = []
         for room in rooms:
             seats.append({
